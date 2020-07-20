501faab2485076657c514437a505a412a65890b2
henrynho89 <henrynho89@gmail.com>
2020-07-20 15:41:39 +0000

    add-router: ajout des routes
    - ajout du module RouterModule
    - ajout des routes dans la méthode forChild
    - ajout des routes home, search, favorite et la route par defaut
    - ajout de la barre de navigation et de son style
    - ajout du grid

--- herosAngular/src/app/app.module.ts
+++ herosAngular/src/app/app.module.ts
@@ -2,13 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SearchComponent } from './search/search.component';
+import { HerosDetailsComponent } from './heros-details/heros-details.component';
+import { HerosListComponent } from './heros-list/heros-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {RouterModule} from '@angular/router';
+import { FavoriteComponent } from './favorite/favorite.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    SearchComponent,
+    HerosDetailsComponent,
+    HerosListComponent,
+    NavbarComponent,
+    FavoriteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([ 
+      {path: '', component:HomeComponent},
+      {path: 'favorite', component:FavoriteComponent},
+      {path: 'search', component:SearchComponent},
+      {path: 'heros-details', component:HerosDetailsComponent},
+      {path: 'profile/:id', component:ProfileComponent},
+      {path: '**', component:HomeComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
